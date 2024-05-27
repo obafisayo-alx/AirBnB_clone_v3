815bfbc1502c5941f45b4a9e56378ecd6d44a2f7
obafisayo-alx <sekaniisjoy@gmail.com>
2024-05-27 23:33:35 +0100

    add task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,17 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
+def get_status():
     """
     status route
     :return: response with json
@@ -19,3 +26,20 @@ def status():
     resp.status_code = 200
 
     return resp
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stat():
+    """
+    retrieves number of each object type
+    Return: return response with json
+    """
+    data = {
+        "amenities": str(storage.count(Amenity)),
+        "cities": str(storage.count(City)),
+        "places": str(storage.count(Place)),
+        "reviews": str(storage.count(Review)),
+        "states": str(storage.count(State)),
+        "users": str(storage.count(User))
+    }
+    return jsonify(data)
+    
