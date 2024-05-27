eec70b92e52a7ff6be3ddf624313bfadc4b49cc9
obafisayo-alx <sekaniisjoy@gmail.com>
2024-05-27 23:48:16 +0100

    updated again o

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,12 +4,6 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -35,11 +29,11 @@ def get_stat():
     Return: return response with json
     """
     data = {
-        "amenities": str(storage.count(Amenity)),
-        "cities": str(storage.count(City)),
-        "places": str(storage.count(Place)),
-        "reviews": str(storage.count(Review)),
-        "states": str(storage.count(State)),
-        "users": str(storage.count(User))
+        "amenities": str(storage.count("Amenity")),
+        "cities": str(storage.count("City")),
+        "places": str(storage.count("Place")),
+        "reviews": str(storage.count("Review")),
+        "states": str(storage.count("State")),
+        "users": str(storage.count("User"))
     }
     return jsonify(data)
