55bcce6991371c3fddd208ef4abed109b5b6d6b5
obafisayo-alx <sekaniisjoy@gmail.com>
2024-05-28 09:50:12 +0100

    updated app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,37 +1,45 @@
 #!/usr/bin/python3
-"""check the status of your API"""
+"""
+app
+"""
 
+from flask import Flask, jsonify
+from flask_cors import CORS
 from os import getenv
-from flask import Flask, jsonify, make_response
+
+from api.v1.views import app_views
 from models import storage
 
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-def create_app():
-    """Create flask application."""
-
-    with app.app_context():
-        from api.v1.views import app_views
-        app.register_blueprint(app_views)
-        return app
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """close db connection when app context is torn down"""
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({"error": "Not found"}), 404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
 
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST")
-    port = getenv("HBNB_API_PORT")
-    app = create_app()
-    if host and port:
-        app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
