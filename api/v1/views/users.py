9e23b70660cb9ec150678ec9d5891694b1a09e63
obafisayo-alx <sekaniisjoy@gmail.com>
2024-05-28 22:26:44 +0100

    removed updates to users view jare

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -2,18 +2,10 @@
 """A view for Users objects"""
 
 
+from xml.etree.ElementInclude import include
 from flask import abort, jsonify, request
 from api.v1.views import app_views, storage
 from models.user import User
-import hashlib
-
-
-def user_to_dict_without_password(user):
-    """Convert user object to dictionary without the password field."""
-    user_dict = user.to_dict()
-    if 'password' in user_dict:
-        del user_dict['password']
-    return user_dict
 
 
 @app_views.route('/users', methods=['GET', 'POST'], strict_slashes=False)
@@ -22,10 +14,9 @@ def users():
     Return: a list of users or created user
     """
     if request.method == 'GET':
-        users_list = [user_to_dict_without_password(user)
+        users_list = [user.to_dict()
                       for user in storage.all(User).values()]
         return jsonify(users_list)
-
     elif request.method == 'POST':
         if not request.is_json:
             abort(400, "Not a JSON")
@@ -36,18 +27,18 @@ def users():
             abort(400, "Missing email")
         if 'password' not in data:
             abort(400, "Missing password")
-
-        data['password'] = hashlib.md5(data['password'].encode()).hexdigest()
         new_user = User(**data)
         new_user.save()
-        res = jsonify(user_to_dict_without_password(new_user))
+        res = jsonify(new_user.to_dict())
         res.status_code = 201
         return res
 
 
-@app_views.route("/users/<user_id>", methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+@app_views.route("/users/<user_id>",
+                 methods=['GET', 'PUT', 'DELETE'],
+                 strict_slashes=False)
 def user_by_id(user_id):
-    """Handles GET, PUT, and DELETE request for /users/<user_id>
+    """Handles GET, PUT and DELETE request for /users/<user_id>
 
     Keyword arguments:
     user_id -- this is the user id
@@ -56,25 +47,17 @@ def user_by_id(user_id):
     user = storage.get(User, user_id)
     if not user:
         abort(404)
-
     if request.method == 'GET':
-        return jsonify(user_to_dict_without_password(user))
-
+        return jsonify(user.to_dict())
     elif request.method == 'PUT':
-        if not request.is_json:
-            abort(400, "Not a JSON")
         data = request.get_json()
         if not data:
             abort(400, "Not a JSON")
-
         for key, val in data.items():
             if key not in ['id', 'created_at', 'updated_at']:
-                if key == 'password':
-                    val = hashlib.md5(val.encode()).hexdigest()
                 setattr(user, key, val)
         user.save()
-        return jsonify(user_to_dict_without_password(user))
-
+        return jsonify(user.to_dict())
     elif request.method == 'DELETE':
         user.delete()
         storage.save()
